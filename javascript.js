d90f275378187fb2af3b7eab86ba5c363502ee6e
E-Wingard <simbulirekiwinga@gmail.com>
2024-08-18 21:06:30 +0300

    i have create afunction that passes choices of both cumper and human and use them to evaluate the winner

--- javascript.js
+++ javascript.js
@@ -26,6 +26,7 @@ function getHumanChoice() {
     /*let userInput = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
 */
 userInput="rock";
+
     while (true) {
         if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
             return userInput;
@@ -35,12 +36,61 @@ userInput="rock";
     }
 }
 
+let humanScore =0;
+let computerScore = 0;
 
 function playGame(humanChoice,computerChoice){
-humanChoice.toLowerCase();
+
 if(humanChoice === computerChoice){
-    return "its atie"
-} else return 'computer wins';
+    return "its atie";
+} 
+    else if (computerChoice === 'paper' && humanChoice === 'rock'){
+      computerScore++;
+      return "You loose,paper beats rock";
     
+       
+    }
+    else if (computerChoice === 'paper' && humanChoice === 'scissor'){
+      humanScore++;
+        return "You win,scissor beats rock";
+      
+  
+      }
+      else if (computerChoice === 'scissor' && humanChoice === 'paper'){
+        computerScore++;
+        return "you lose,scissor beats rock";
+      
+      }
+
+      else if (computerChoice === 'scissor' && humanChoice === 'rock'){
+        humanScore++;
+        return " you win,scissor beats rock" ;
+       
+  
+      }
+      else if (computerChoice === 'rock' && humanChoice === 'paper'){
+        humanScore++;
+        return "you win,paper beats rock";
+      
+  
+      }
+
+      else if (computerChoice === 'rock' && humanChoice === 'scissor'){
+        computerScore++;
+        return "you lose,scissor beats rock";
+     
+  
+      } else
+      
+      { return "Error occured";
+
+      }
+     
 }
+
 console.log(playGame(getHumanChoice(), getComputerChoice ()));
+
+console.log(`computerScore:${computerScore}, YOU:${humanScore}`);
+
+
+
