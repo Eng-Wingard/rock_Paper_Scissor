23b84681beffae8457bfaefeef42d4f7c5eb3abb
E-Wingard <simbulirekiwinga@gmail.com>
2024-08-18 05:01:26 +0300

    This takes i human choice and comuter choices and return the winner

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 function getComputerChoice () {
+
 let randomNumber = Math.round(Math.random()* 3);
 
 //switch case statement to return either rock or scissor or paper
@@ -17,3 +18,29 @@ return 'scissor';
 } 
 
 }
+
+
+//userInput
+
+function getHumanChoice() {
+    /*let userInput = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+*/
+userInput="rock";
+    while (true) {
+        if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+            return userInput;
+        } else {
+            /*userInput = prompt("Invalid choice. Please enter 'rock', 'paper', or 'scissors':").toLowerCase(); */
+        }
+    }
+}
+
+
+function playGame(humanChoice,computerChoice){
+humanChoice.toLowerCase();
+if(humanChoice === computerChoice){
+    return "its atie"
+} else return 'computer wins';
+    
+}
+console.log(playGame(getHumanChoice(), getComputerChoice ()));
